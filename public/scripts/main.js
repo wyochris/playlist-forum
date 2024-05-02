8dcfdd8f2455aee18ebb6e6b5789960f075335cb
rhit-zhangl8 <112708732+rhit-zhangl8@users.noreply.github.com>
2024-05-01 21:12:45 -0400

    second page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,13 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	document.getElementById("songsButton").onclick = (event) => {
+		window.location.href = `/songs.html`;
+	};
+	document.getElementById("playlistsButton").onclick = (event) => {
+		window.location.href = `/index.html`;
+	};
 };
 
 rhit.main();
