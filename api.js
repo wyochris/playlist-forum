672beb67ee42e1859ecb061375d7e948f6d964f4
lardnece <lardnece@rose-hulman.edu>
2024-05-09 11:19:56 -0400

    more api results

--- api.js
+++ api.js
@@ -5,6 +5,30 @@ const SpotifyWebApi = require("spotify-web-api-node");
 
 const express = require('express');
 var app = express();
+const crypto = require('crypto');
+const querystring = require('querystring');
+
+const scopes = [
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
+];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,13 +39,67 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: process.env.REDIRECT_URL
 });
 
-// Retrieve an access token (test)
-spotifyApi
-  .clientCredentialsGrant()
-  .then(data => {
-    console.log(data.body)
-    spotifyApi.setAccessToken(data.body["access_token"]);
-  })
-  .catch(error => {
-    console.log("Something went wrong when retrieving an access token", error);
-  });
+app.get('/login', (req, res) => {
+  res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+app.get('/callback', (req, res) => {
+  const error = req.query.error;
+  const code = req.query.code;
+  const state = req.query.state;
+
+  if (error) {
+    console.error('Callback Error:', error);
+    res.send(`Callback Error: ${error}`);
+    return;
+  }
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+
+      console.log('access_token:', access_token);
+      console.log('refresh_token:', refresh_token);
+
+      console.log(
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+      );
+      res.send('Success! You can now close the window.');
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body['access_token'];
+
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
+        spotifyApi.setAccessToken(access_token);
+      }, expires_in / 2 * 1000);
+
+      // TODO: Link up with Client Side
+      return spotifyApi.searchTracks('Love');
+
+    })
+    .then(function(data) {
+      console.log(data.body.tracks.total + ' tracks');
+  
+      // Go through the first page of results
+      var firstPage = data.body.tracks.items;
+      console.log('The first page are (popularity in parentheses):');
+
+      firstPage.forEach(function(track, index) {
+        console.log(index + ': ' + track.name + ' (' + track.artists[0].name + ')');
+      });
+    }).catch(error => {
+      console.error('Error:', error);
+      res.send(`Error: ${error}`);
+    });
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
