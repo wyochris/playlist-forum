7593ab66ce09035ffa83567c54cf2e6d8a36d98b
lardnece <lardnece@rose-hulman.edu>
2024-05-15 21:39:47 -0400

    resultspageController

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -137,8 +137,6 @@ PlaylistManager = class {
 	}
 }
 
-
-
 main = function () {
 	console.log("Ready");
 	document.getElementById("songsButton").onclick = (event) => {
@@ -150,52 +148,62 @@ main = function () {
 	document.getElementById("detailsButton").onclick = (event) => {
 		window.location.href = `/details.html`;
 	};
+	document.getElementById("resultsButton").onclick = (event) => {
+		window.location.href = `/results.html`;
+	};
 };
 
-// Function to fetch Spotify data and update song cards
-function updateSongCards(searchQuery) {
-	console.log('querying api');
-	fetch(`http://localhost:5001/lardner-zhang-final-csse280/us-central1/api/search/${searchQuery}`)
-	  .then(response => response.json())
-	  .then(data => {
-		const searchResultsContainer = document.getElementById('searchResults');
-		searchResultsContainer.innerHTML = ''; // Clear previous content
-		console.log('data fetching');
-		var firstPage = data.body.tracks.items;
-		console.log('I got ' + data.body.tracks.total + ' results!');
-		firstPage.forEach(function(track, index) {
-			console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
-		  });
+ResultPageController = class {
+	constructor() {
+		document.getElementById("searchForm").onsubmit = (event) => {
+			handleSearch();
+		};
+	}
 
-		data.forEach(track => {
-		  const card = document.createElement('div');
-		  card.className = 'card';
-		  const cardBody = document.createElement('div');
-		  cardBody.className = 'card-body';
-		  const image = document.createElement('img');
-		  image.src = track.album.images[0].url;
-		  image.className = 'img-fluid';
-		  const songTitle = document.createElement('p');
-		  songTitle.textContent = track.name;
+	handleSearch() {
+		const searchQuery = document.getElementById('searchInput').value;
+		updateSongCards(searchQuery);
+		console.log('handle search finish');
+	}
 
-		  console.log(track.name);
-		  
-		  cardBody.appendChild(image);
-		  cardBody.appendChild(songTitle);
-		  card.appendChild(cardBody);
-		  searchResultsContainer.appendChild(card);
-		});
-	  })
-	  .catch(error => console.error('Error updating song cards:', error));
+	updateSongCards(searchQuery) {
+		console.log('querying api');
+		fetch(`http://localhost:5001/lardner-zhang-final-csse280/us-central1/api/search/${searchQuery}`)
+		.then(response => response.json())
+		.then(data => {
+			const searchResultsContainer = document.getElementById('searchResults');
+			searchResultsContainer.innerHTML = ''; // Clear previous content
+			console.log('data fetching');
+			var firstPage = data.body.tracks.items;
+			console.log('I got ' + data.body.tracks.total + ' results!');
+			firstPage.forEach(function(track, index) {
+				console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
+			});
 
-	console.log("fetch finish");
-  }
-  
-  // when search button clicked
-function handleSearch() {
-	const searchQuery = document.getElementById('searchInput').value;
-	window.location.href = `http://localhost:5000//results.html?query=${encodeURIComponent(searchQuery)}`;
-  }
-  
+			data.forEach(track => {
+			const card = document.createElement('div');
+			card.className = 'card';
+			const cardBody = document.createElement('div');
+			cardBody.className = 'card-body';
+			const image = document.createElement('img');
+			image.src = track.album.images[0].url;
+			image.className = 'img-fluid';
+			const songTitle = document.createElement('p');
+			songTitle.textContent = track.name;
+
+			console.log(track.name);
+			
+			cardBody.appendChild(image);
+			cardBody.appendChild(songTitle);
+			card.appendChild(cardBody);
+			searchResultsContainer.appendChild(card);
+			});
+		})
+		.catch(error => console.error('Error updating song cards:', error));
+
+		console.log("fetch finish");
+	}
+
+}
 
 main();
